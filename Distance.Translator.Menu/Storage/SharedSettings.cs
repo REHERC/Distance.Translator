0453b07c69a33068cc33b9a0a046b512b930f0f4
REHERC <amaury.vanoorenberghe@yahoo.com>
2018-09-24 19:59:48 +0200

    Wiped all the code to rewrite the mod from scratch (rip in peace old version roflmao)

--- Distance.Translator.Menu/Storage/SharedSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Distance.Translator.Menu
-{
-    public static partial class SharedSettings
-    {
-        public static bool RAINBOW_MODE;
-        public static string CURRENT_LANGUAGE;
-        public static bool MAINPLUGIN_DETECTED = false;
-    }
-}

60ef519c3e4b65e7816baa99d8918622596deb2a
REHERC <amaury.vanoorenberghe@yahoo.com>
2019-03-24 22:52:00 +0100

    Started to add a redirection table from the csv subtitle names to the json ones

--- Distance.Translator/T4/Subtitle/SubtitleIdentifiers.cs
+++ Distance.Translator/T4/Subtitle/SubtitleIdentifiers.cs
@@ -6,6 +6,54 @@ namespace Distance.Translator.T4
 {
 	public static class SubtitleIdentitiers
 	{
-		static Dictionary<string, string> Bindings { get; } = new Dictionary<string, string>();
+		static Dictionary<string, string> Bindings { get; } = new Dictionary<string, string>()
+		{
+			{"cb_happy_chime/0", "subtitles.cb_happy_chime"},
+			{"cb_diversion1a_jan2018/1", "subtitles.cb_diversion1a.0"},
+			{"cb_diversion1a_jan2018/2", "subtitles.cb_diversion1a.1"},
+			{"cb_diversion1a_jan2018/0", "subtitles.cb_diversion1a.2"},
+			{"cb_diversion1b_jan2018/1", "subtitles.cb_diversion1b.0"},
+			{"cb_diversion1b_jan2018/2", "subtitles.cb_diversion1b.1"},
+			{"City Broadcast Error Chime v2", "subtitles.cb_error_chime"},
+			{"Numbers 3", "subtitles.cb_numbers"},
+			{"cb_cataclysm1_jan2018/1", "subtitles.cb_cataclysm1.0"},
+			{"cb_cataclysm1_jan2018/2", "subtitles.cb_cataclysm1.1"},
+			{"cb_cataclysm1_jan2018/3", "subtitles.cb_cataclysm1.2"},
+			{"cb_cataclysm1_jan2018/4", "subtitles.cb_cataclysm1.3"},
+			{"cb_cataclysm1_jan2018/5", "subtitles.cb_cataclysm1.4"},
+			{"cb_diversion2_jan2018/1", "subtitles.cb_diversion2.0"},
+			{"cb_diversion2_jan2018/2", "subtitles.cb_diversion2.1"},
+			{"cb_diversion2_jan2018/3", "subtitles.cb_diversion2.2"},
+			{"cb_diversion2_jan2018/4", "subtitles.cb_diversion2.3"},
+			{"cb_diversion2_jan2018/5", "subtitles.cb_diversion2.4"},
+			{"cb_diversion2_jan2018/6", "subtitles.cb_diversion2.5"},
+			{"cb_cataclysm2a_jan2018/0", "subtitles.cb_cataclysm2a.0"},
+			{"cb_cataclysm2b_jan2018/4", "subtitles.cb_cataclysm2b.0"},
+			{"cb_cataclysm2b_jan2018/2", "subtitles.cb_cataclysm2b.1"},
+			{"cb_cataclysm2b_jan2018/3", "subtitles.cb_cataclysm2b.2"},
+			{"cb_cataclysm2c_jan2018/1", "subtitles.cb_cataclysm2c.0"},
+			{"cb_cataclysm2c_jan2018/2", "subtitles.cb_cataclysm2c.1"},
+			{"cb_euphoria1_jan2018/1", "subtitles.cb_euphoria1.0"},
+			{"cb_euphoria1_jan2018/2", "subtitles.cb_euphoria1.1"},
+			{"cb_euphoria1_jan2018/3", "subtitles.cb_euphoria1.2"},
+			{"cb_euphoria2_jan2018/1", "subtitles.cb_euphoria2.0"},
+			{"cb_euphoria2_jan2018/2", "subtitles.cb_euphoria2.1"},
+			{"cb_euphoria2_jan2018/3", "subtitles.cb_euphoria2.2"},
+			{"cb_entanglement1_jan2018/1", "subtitles.cb_entanglement1.0"},
+			{"cb_entanglement1_jan2018/2", "subtitles.cb_entanglement1.1"},
+			{"cb_entanglement1_jan2018/3", "subtitles.cb_entanglement1.2"},
+			{"cb_entanglement1_jan2018/4", "subtitles.cb_entanglement1.3"},
+			{"cb_entanglement2_jan2018/1", "subtitles.cb_entanglement2.0"},
+			{"cb_entanglement2_jan2018/2", "subtitles.cb_entanglement2.1"},
+			{"cb_entanglement2_jan2018/3", "subtitles.cb_entanglement2.2"},
+			{"cb_entanglement3_jan2018/1", "subtitles.cb_entanglement3.0"},
+			{"cb_entanglement3_jan2018/2", "subtitles.cb_entanglement3.1"},
+			{"cb_entanglement3_jan2018/3", "subtitles.cb_entanglement3.2"},
+			{"cb_entanglement4_jan2018/1", "subtitles.cb_entanglement4.0"},
+			{"cb_entanglement4_jan2018/2", "subtitles.cb_entanglement4.1"},
+			{"cb_entanglement4_jan2018/3", "subtitles.cb_entanglement4.2"},
+			{"cb_entanglement4_jan2018/4", "subtitles.cb_entanglement4.3"},
+			{"cb_entanglement4_jan2018/5", "subtitles.cb_entanglement4.4"}
+		};
 	}
 }
